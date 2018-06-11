d8cbb0f3aa7e530c25fb63e15d4eb079cfd3b769
astrastar <37809311+astrastar@users.noreply.github.com>
2018-06-11 02:05:59 -0400

    new

---
+++ test_scripts/client_r.py
@@ -0,0 +1,45 @@
+from socket import *
+import json
+import sys
+
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect(('localhost', 8885))
+
+def make_pres_msg():
+    pres_msg = {
+        'action': 'presence',
+        'type': 'online'
+    }
+    return pres_msg
+
+def msg_encode(msg):
+    j_msg = json.dumps(msg)
+    b_msg = j_msg.encode('utf-8')
+    return b_msg
+
+
+def msg_decode(msg):
+    b_msg = msg.decode('utf-8')
+    j_msg = json.loads(b_msg)
+    return j_msg
+
+
+def send_msg(msg):
+    s.send(msg_encode(msg))
+    return None
+
+
+def recv_msg():
+    response = s.recv(1024)
+    msg = msg_decode(response)#.get('text')
+    print(msg)
+    return msg
+
+if __name__ == '__main__':
+
+    msg = make_pres_msg()
+    send_msg(msg)
+    recv_msg()
+
+    s.close()
